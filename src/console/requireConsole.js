f45b12bad38ac0895ec0a55087b6a0f919e6b091
Davis W. Frank <dwfrank+github@infe.ws>
2013-05-17 23:30:37 -0700

    Manual requires/modules now node_suite.js compatible

--- src/console/requireConsole.js
+++ src/console/requireConsole.js
@@ -1,3 +1,9 @@
+var jasmineRequire = jasmineRequire || {};
+
 jasmineRequire.console = function(j$) {
   j$.ConsoleReporter = jasmineRequire.ConsoleReporter();
 };
+
+if (typeof exports == "object") {
+  exports.jasmineRequire = jasmineRequire;
+}
